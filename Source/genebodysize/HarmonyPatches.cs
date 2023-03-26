79d39bc9525ebf8e152ce1df3ea71055fbb3aaad
oorzkws <65210810+oorzkws@users.noreply.github.com>
2023-03-26 11:15:59 -0600

    VariedBodySize current version compat

--- Source/genebodysize/HarmonyPatches.cs
+++ Source/genebodysize/HarmonyPatches.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Reflection;
-using HarmonyLib;
+﻿using HarmonyLib;
 using RimWorld;
-using UnityEngine;
 using VariedBodySizes;
 using Verse;
 
@@ -12,52 +9,28 @@ namespace genebodysize;
 
 public class HarmonyPatches
 {
-    [HarmonyPatch]
-    public static class VariedBodySizesMod_SettingsPatch
-    {
-        // Since it's internal we need to do a bit of reflection
-        static MethodBase TargetMethod()
-        {
-            var typeByName = AccessTools.TypeByName("VariedBodySizes.VariedBodySizesMod");
-            return AccessTools.PropertyGetter(typeByName, "Settings");
-        }
-
-        public static void Postfix(ref VariedBodySizesModSettings __result)
-        {
-            __result.AffectMeleeDamage = true;
-            __result.AffectRealBodySize = true;
-            __result.AffectRealHealthScale = true;
-            __result.AffectMeleeDodgeChance = true;
-            // Only override if default
-            if (!__result.VariedBodySizes.ContainsKey("Human") ||
-                __result.VariedBodySizes["Human"] == new FloatRange(0.9f, 1.1f))
-                __result.VariedBodySizes["Human"] = new FloatRange(0.95f, 1.05f);
-        }
-
-    }
-    
     [HarmonyPatch(typeof(VariedBodySizes_GameComponent), "OnCalculateBodySize")]
     public static class Pawn_BodySizePatch_OnCalculateBodySizePatch
     {
-        private static readonly TimedCache<float> statCache = new(360);
+        private static readonly TimedCache<float> statCache = new(36);
         private static StatDef bodySizeMultiplier;
         // Register a modifier for ez access
-        public static float Postfix(float result, Pawn pawn)
+        public static void Postfix(ref float __result, Pawn pawn)
         {
             // GetStatValue will access needs, which if null, will break loading /tableflip
             if (!ModsConfig.BiotechActive || pawn?.RaceProps.Humanlike != true || pawn.needs == null && !pawn.Dead)
-                return result;
-            
+                return;
+
             // cached value, or calculate/cache/return
-            if (statCache.TryGet(pawn, out var pawnSize))
-            {
-                return pawnSize;
+            if (statCache.TryGet(pawn, out var pawnSize) && pawnSize > 0.01f) {
+                __result = pawnSize;
+                return;
             }
         
             // Load-time fun!
             bodySizeMultiplier ??= StatDef.Named("bodySizeFactor");
-
-            return statCache.SetAndReturn(pawn, result * pawn.GetStatValue(bodySizeMultiplier, cacheStaleAfterTicks: 3600));
+            var bodySizeMultiplierValue = pawn.GetStatValue(bodySizeMultiplier, cacheStaleAfterTicks: 360);
+            statCache.Set(pawn, __result *= bodySizeMultiplierValue);
         }
         
     }
