64d1a5cc3cccafdc68ae4828b453cbd422cd2e2a
oorzkws <65210810+oorzkws@users.noreply.github.com>
2023-05-20 03:03:31 -0600

    Cleanup

--- Source/genebodysize/Main.cs
+++ Source/genebodysize/Main.cs
@@ -1,15 +1,11 @@
 ﻿using System.Reflection;
 using HarmonyLib;
-using RimWorld;
-using VariedBodySizes;
 using Verse;
 
 namespace genebodysize;
 [StaticConstructorOnStartup]
 public static class Main
 {
-    public static GeneSizeGameComponent Component;
-    
     static Main()
     {
         new Harmony("mute.genebodysize").PatchAll(Assembly.GetExecutingAssembly());
